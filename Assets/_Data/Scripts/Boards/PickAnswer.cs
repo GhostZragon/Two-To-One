00c32e7fba7f4f6097df26aa93d0913d5c3bf59e
Quang <quangviet932@gmail.com>
2023-04-14 17:28:54 +0700

    Add ref
    Auto ref with board and calculator system

--- Assets/_Data/Scripts/Boards/PickAnswer.cs
+++ Assets/_Data/Scripts/Boards/PickAnswer.cs
@@ -2,25 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PickAnswer : MonoBehaviour
+public class PickAnswer : QuangLibrary
 {
     public Board board;
+    public CalculatorSystem calculator;
+    protected override void LoadComponent()
+    {
+        base.LoadComponent();
+        this.LoadCalculatorSystem();
+        this.LoadBoard();
+    }
+    protected virtual void LoadCalculatorSystem()
+    {
+        if (calculator != null) return;
+        calculator = FindObjectOfType<CalculatorSystem>();
+    }   
+    protected virtual void LoadBoard()
+    {
+        if (board != null) return;
+        board = GameObject.Find("BoardManager").GetComponent<Board>();
+    }
     public int PickRandom()
     {
-
         List<Transform> list = new List<Transform>();
         list = board.clickableCell;
         if (list.Count == 0) return 0;
-        int a = Random.Range(0,list.Count);
+        int a = Random.Range(0, list.Count);
         int b = Random.Range(0, list.Count);
-        while(a == b)
+        while (a == b)
         {
             a = Random.Range(0, list.Count);
         }
         Cell cell01 = list[a].GetComponent<Cell>();
         Cell cell02 = list[b].GetComponent<Cell>();
-        int c = cell01.infor.value + cell02.infor.value;
+        int a_value = cell01.infor.value, b_value = cell02.infor.value;
+        int c = a_value + b_value;
+
         Debug.Log($"{cell01.infor.value} + {cell02.infor.value} = {c}");
         return c;
+    }
+
+
+    void RandomNumber()
+    {
+
     }
 }
